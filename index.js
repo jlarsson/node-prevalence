1090aa9cf712bb4027e9c1c082cfeb835bb8e3d2
Joakim Larsson <mimmertim@gmail.com>
2015-03-23 15:38:04 +0100

    :see_no_evil:
    - away with ;
    - some currying to speed up closures
    - some assertion messages

--- index.js
+++ index.js
@@ -1,78 +1,80 @@
 (function(module) {
-  'use strict';
+  'use strict'
 
-  var Promise = require('native-or-bluebird');
-  var co = require('co');
-  var thenify = require('thenify');
-  var costream = require('co-stream')
-  var debug = require('debug')('prevalence');
-  var rwlock = require('rwlock');
-  var defaults = require('defaults');
-  var jsonstream = require('json-stream')
-  var fs = require('mz/fs');
-  var path = require('path');
-  var mkdirp = thenify(require('mkdirp'));
-  var util = require('util');
-  var makeError = require('make-error');
+  var thenify = require('thenify'),
+    co = require('co'),
+    costream = require('co-stream'),
+    debug = require('debug')('prevalence'),
+    defaults = require('defaults'),
+    fs = require('mz/fs'),
+    jsonstream = require('json-stream'),
+    makeError = require('make-error'),
+    mkdirp = thenify(require('mkdirp')),
+    path = require('path'),
+    Promise = require('native-or-bluebird'),
+    rwlock = require('rwlock'),
+    util = require('util')
 
-  module.exports = function(options) { return new Repository(options); }
-  var ConfigurationError = module.exports.ConfigurationError = makeError('ConfigurationError');
-  var CommandError = module.exports.CommandError = makeError('CommandError');
+  module.exports = function(options) {
+    return new Repository(options)
+  }
+  var ConfigurationError = module.exports.ConfigurationError = makeError('ConfigurationError')
+  var CommandError = module.exports.CommandError = makeError('CommandError')
 
   function Repository(options) {
     options = defaults(options, {
       model: {}
-    });
+    })
     if (!options.path) {
       throw new ConfigurationError('Repository requires a path to a journalfile.\r\nUsage: require(\'prevalence\')({path:<path to log>})')
     }
 
     // resolve journal path relative to root
-    var journalPath = path.resolve(path.dirname(require.main.filename), options.path);
-    debug('journalling to %s', journalPath);
+    var journalPath = path.resolve(path.dirname(require.main.filename), options.path)
+    debug('journalling to %s', journalPath)
 
-    var model = options.model;
-    debug('initial model is %j', model);
+    var model = options.model
+    debug('initial model is %j', model)
 
-    var isInitialized = false;
-    var commands = {};
+    var isInitialized = false
+    var commands = {}
 
-    var lock = createLockObject();
-    var marshal = createMarshalFunction();
-    var appendToJournal = createJournalAppender(journalPath);
+    var lock = createLockObject()
+    var marshal = createMarshalFunction()
+    var appendToJournal = createJournalAppender(journalPath)
 
     // Register a match-unmapped-commands handler
-    register('*', unregisteredCommandHandler);
+    register('*', unregisteredCommandHandler)
 
     // Public methods
-    this.register = register;
-    this.query = query;
-    this.execute = execute;
+    this.register = register
+    this.query = query
+    this.execute = execute
 
     function register(name, executeFn) {
-      commands[name] = executeFn;
-      return this;
+      commands[name] = executeFn
+      return this
     }
 
     function getCommand(name) {
       if (commands.hasOwnProperty(name)) {
-        return commands[name];
+        return commands[name]
       }
       if (commands.hasOwnProperty('*')) {
-        return commands['*'];
+        return commands['*']
       }
 
-      return unregisteredCommandHandler;
+      return unregisteredCommandHandler
     }
 
     function query(queryFn) {
       return init()
         .then(function() {
           return lock.read(function*() {
-            var res = yield queryFn(model);
-            return marshal(res);
-          });
-        });
+            var res = yield queryFn(model)
+            return marshal(res)
+          })
+        })
     }
 
     function execute(name, arg) {
@@ -84,57 +86,54 @@
               name: name,
               arg: arg,
               replay: false
-            };
-            yield appendToJournal(command);
-            var res = yield exec(command);
-            return marshal(res);
+            }
+            yield appendToJournal(command)
+            var res = yield exec(command)
+            return marshal(res)
           })
-        });
+        })
     }
 
     function init() {
       if (isInitialized) {
-        return Promise.resolve(true);
+        return Promise.resolve(true)
       }
       return lock.write(function*() {
         if (isInitialized) {
-          return Promise.resolve(true);
+          return Promise.resolve(true)
         }
-        debug('initializing');
-        var replayCount = 0;
+        debug('initializing')
+        var replayCount = 0
 
-        return yield createJournalReader(journalPath, replayExec)
+        return yield createJournalReader(journalPath, replayExec.bind(null, model, exec))
           .then(function() {
-            isInitialized = true;
-            debug('initialization done. replayed commands: %s', replayCount);
+            isInitialized = true
+            debug('initialization done. replayed commands: %s', replayCount)
           })
           .catch(function(err) {
-            debug('initialization failed: %s', err);
-            throw err;
-          });
+            debug('initialization failed: %s', err)
+            throw err
+          })
 
-        function replayExec(record) {
-          ++replayCount;
-          return exec({
-            model: model,
-            name: record.n,
-            arg: record.arg,
-            replay: true
-          });
+        function replayExec(model, exec, command) {
+          ++replayCount
+          // Fill in model since its not persisted in journal (only name and arg)
+          command.model = model
+          return exec(command)
         }
-      });
+      })
     }
 
     function exec(command) {
-      return getCommand(command.name)(command.model, command.arg, command);
+      return getCommand(command.name)(command.model, command.arg, command)
     }
 
     // Readers/Writers lock factory
     function createLockObject() {
-      var lck = new rwlock();
+      var lck = new rwlock()
       return {
         read: lock.bind(null, 'readLock'),
-        write: lock.bind(null, 'writeLock'),
+        write: lock.bind(null, 'writeLock')
       }
 
       function lock(type, gen) {
@@ -142,34 +141,35 @@
           lck[type](function(release) {
             co(gen)
               .then(function(res) {
-                resolve(res);
-                release();
+                resolve(res)
+                release()
               })
               .catch(function(err) {
-                reject(err);
-                release();
+                reject(err)
+                release()
               })
-          });
+          })
         })
-      };
+      }
     }
 
     // The purpose of a marshaller is to create a deep copy, so that
     // modifications to values returned from execute()/read() doesnt jeopardize
     // the stability/correctness of the managed prevalent model.
     function createMarshalFunction() {
-      var marshalSkipTypes = {
+      var dontMarshalTheseScalarTypes = {
         "undefined": true,
         "boolean": true,
         "number": true,
-        "string": true
-      };
-      return function marshal(value) {
-        if ((value === null) || marshalSkipTypes[typeof(value)]) {
-          return value;
-        }
-        return typeof(value.marshal) === 'function' ? value.marshal() : JSON.parse(JSON.stringify(value));
+        "string": true,
+        "function": true
       }
+      return function marshal(skip, value) {
+        if ((value === null) || skip[typeof(value)]) {
+          return value
+        }
+        return typeof(value.marshal) === 'function' ? value.marshal() : JSON.parse(JSON.stringify(value))
+      }.bind(null, dontMarshalTheseScalarTypes)
     }
 
     // A journal appender appends a json snapshot of command characteristics
@@ -179,29 +179,29 @@
           t: new Date(),
           n: command.name,
           a: command.arg
-        }) + '\n';
+        }) + '\n'
 
         return co(function*() {
           try {
-            return yield append();
+            return yield append()
           } catch (e) {
             if (e && e.code === 'ENOENT') {
               // the journal file doesnt exists nor can it be created
               // assume its cuz a missing folder
-              debug('creating journalling folder');
-              yield mkdirp(path.dirname(journalPath));
-              return yield append();
+              debug('creating journalling folder')
+              yield mkdirp(path.dirname(journalPath))
+              return yield append()
             }
-            throw e;
+            throw e
           }
-        });
+        })
 
         function append() {
           return fs.appendFile(journalPath, line, {
             encoding: 'utf8'
-          });
+          })
         }
-      }.bind(null, journalPath);
+      }.bind(null, journalPath)
     }
 
 
@@ -215,30 +215,34 @@
         return fs.createReadStream(journalPath).on('error', onReadStreamError)
           .pipe(new jsonstream()).on('error', reject)
           .pipe(costream.each(function*(record) {
-            yield exec(record)
+            yield exec({
+              name: record.n,
+              arg: record.a,
+              replay: true
+            })
           }, {
             objectMode: true,
             parallell: 1
           }))
           .on('error', reject)
           .on('finish', function() {
-            resolve(true);
-          });
+            resolve(true)
+          })
 
         function onReadStreamError(err) {
           if (err && err.code == 'ENOENT') {
             // A missing journal file is equivalent to an empty one
-            return resolve(true);
+            return resolve(true)
           }
-          reject(err);
+          reject(err)
         }
       })
     }
 
     // Handle unregistered commands by throwing
-    function* unregisteredCommandHandler(model, arg, ctx){
-      throw new CommandError(util.format('Unregistered command \'%s\'.\nFix: var repo = require(\'prevalence\').register(\'%s\', function *(){ ... })', ctx.name, ctx.name));
+    function* unregisteredCommandHandler(model, arg, ctx) {
+      throw new CommandError(util.format('Unregistered command \'%s\'.\nFix: var repo = require(\'prevalence\').register(\'%s\', function *(){ ... })', ctx.name, ctx.name))
     }
 
   }
-})(module);
+})(module)
--- test/test.js
+++ test/test.js
@@ -1,101 +1,101 @@
-var assert = require('assert');
-var co = require('co');
-var prevalence = require('../index');
-var fs = require('mz/fs');
-var path = require('path');
+var assert = require('assert'),
+  co = require('co'),
+  fs = require('mz/fs'),
+  path = require('path'),
+  prevalence = require('../index')
 
 describe('prevalence', function() {
-  var journalPath = path.resolve(__dirname, '../tmp/testjournal.log');
+  var journalPath = path.resolve(__dirname, '../tmp/testjournal.log')
 
-  beforeEach(cleanRepo);
-  afterEach(cleanRepo);
+  beforeEach(cleanRepo)
+  afterEach(cleanRepo)
 
   describe('query(* -> yield)', function() {
-    var repo = createRepo();
+    var repo = createRepo()
 
     co_it('with literal', function*() {
       var res = yield repo.query(function*() {
-        return 123;
-      });
-      assert.equal(123, res);
-    });
+        return 123
+      })
+      assert.equal(123, res)
+    })
 
     co_it('with promise', function*() {
       var res = yield repo.query(function*() {
-        return Promise.resolve(123);
-      });
-      assert.equal(123, res);
-    });
+        return Promise.resolve(123)
+      })
+      assert.equal(123, res)
+    })
 
     co_it('can be run in parallell (readlock)', function*() {
-      var maxActive = 0;
-      var active = 0;
+      var maxActive = 0
+      var active = 0
       yield [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
       .map(function(i) {
         return repo.query(function*() {
           return new Promise(function(resolve) {
-            ++active;
-            maxActive = Math.max(active, maxActive);
+            ++active
+            maxActive = Math.max(active, maxActive)
             setTimeout(function() {
-              --active;
-              resolve(i);
-            }, 30);
-          });
-        });
-      });
-      assert.equal(0, active);
-      assert.equal(10, maxActive);
-    });
+              --active
+              resolve(i)
+            }, 30)
+          })
+        })
+      })
+      assert.equal(0, active)
+      assert.equal(10, maxActive)
+    })
 
     co_it('marshals the result', function*() {
       var modelValue = {
         a: 123
-      };
+      }
       var value = yield repo.query(function*() {
-        return modelValue;
-      });
-      assert.deepEqual(modelValue, value);
-      assert.ok(modelValue !== value, "Result should be a copy");
-    });
-  });
+        return modelValue
+      })
+      assert.deepEqual(modelValue, value, "Expected structural equivalence")
+      assert.ok(modelValue !== value, "Should be a copy and not same instance")
+    })
+  })
 
   describe('execute(* -> yield)', function() {
 
     co_it('command must be registered', function*() {
       try {
-        yield createRepo().execute('some unregistered command');
-        assert.fail('expected throw');
+        yield createRepo().execute('some unregistered command')
+        assert.fail('illegal code, expected throw on previous line due to execute of unregistered command')
       } catch (err) {
-        assert.ok(err instanceof prevalence.CommandError);
+        assert.ok(err instanceof prevalence.CommandError, "Expcted CommandError")
       }
-    });
+    })
 
     co_it('commands works (!)', function*() {
       var res = yield createRepo()
         .register('cmd', function*(model, arg, ctx) {
-          return 123 * arg;
+          return 123 * arg
         })
-        .execute('cmd', 3);
+        .execute('cmd', 3)
 
-      assert.equal(3 * 123, res);
-    });
+      assert.equal(3 * 123, res)
+    })
 
     co_it('marshals the result', function*() {
       var modelValue = {
         a: 123
-      };
+      }
       var value = yield createRepo()
         .register('cmd', function*() {
-          return modelValue;
+          return modelValue
         })
-        .execute('cmd');
-      assert.deepEqual(modelValue, value);
-      assert.ok(modelValue !== value);
-    });
+        .execute('cmd')
+      assert.deepEqual(modelValue, value, "Expected structural equivalence")
+      assert.ok(modelValue !== value, "Should be a copy and not same instance")
+    })
 
     co_it('are executed serially (writelock)', function*() {
-      var maxActive = 0;
-      var active = 0;
+      var maxActive = 0
+      var active = 0
 
       var repo = createRepo({
           model: {
@@ -104,22 +104,22 @@ describe('prevalence', function() {
         })
         .register('inc-counter', function*(model) {
           return new Promise(function(resolve) {
-            ++active;
-            maxActive = Math.max(active, maxActive);
+            ++active
+            maxActive = Math.max(active, maxActive)
             setTimeout(function() {
-              --active;
-              resolve(++model.counter);
-            }, 30);
-          });
-        });
+              --active
+              resolve(++model.counter)
+            }, 30)
+          })
+        })
 
       yield [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
       .map(function(i) {
-        return repo.execute('inc-counter');
-      });
-      assert.equal(0, active);
-      assert.equal(1, maxActive);
-    });
+        return repo.execute('inc-counter')
+      })
+      assert.equal(0, active)
+      assert.equal(1, maxActive)
+    })
 
     co_it('invokations are logged to/replayed from journal', function*() {
       function connect() {
@@ -129,29 +129,29 @@ describe('prevalence', function() {
             }
           })
           .register('inc-counter', function*(model) {
-            return ++model.counter;
-          });
+            return ++model.counter
+          })
       }
 
       // Create repository with a registered command
-      var repo = connect();
-      // Run command a couple of times
+      var repo = connect()
+        // Run command a couple of times
       for (var i = 0; i < 10; ++i) {
-        yield repo.execute('inc-counter');
+        yield repo.execute('inc-counter')
       }
       assert.equal(10, yield repo.query(function*(model) {
-        return model.counter;
-      }));
+        return model.counter
+      }))
 
       // Forget repo for a while
-      repo = null;
-      // ...and then come back with full history
+      repo = null
+        // ...and then come back with full history
       var counter = yield connect()
         .query(function*(model) {
-          return model.counter;
-        });
-      assert.equal(10, counter);
-    });
+          return model.counter
+        })
+      assert.equal(10, counter)
+    })
 
     co_it('command effects are persisted between sessions', function*() {
       function connect() {
@@ -161,36 +161,55 @@ describe('prevalence', function() {
             }
           })
           .register('inc-counter', function*(model) {
-            return ++model.counter;
-          });
+            return ++model.counter
+          })
       }
 
       // Connect, execute and detach some times
       for (var i = 0; i < 10; ++i) {
-        yield connect().execute('inc-counter');
+        yield connect().execute('inc-counter')
       }
       // Connect again and check counter
       var counter = yield connect().query(function*(model) {
-        return model.counter;
-      });
-      assert.equal(10, counter);
+        return model.counter
+      })
+      assert.equal(10, counter)
     })
-  });
+
+    co_it('command arguments are persisted between sessions', function*() {
+      function connect() {
+        return createRepo({
+            model: {
+              name: ''
+            }
+          })
+          .register('set-name', function*(model, name) {
+            return model.name = name
+          })
+      }
+      yield connect().execute('set-name', 'testing')
+
+      var name = yield connect().query(function*(model) {
+        return model.name
+      })
+      assert.equal('testing', name)
+    })
+  })
 
   function co_it(description, gen) {
     return it(description, function(done) {
-      co(gen).then(done).catch(done);
+      co(gen).then(done).catch(done)
     })
   }
 
   function createRepo(options) {
-    (options || (options = {})).path = journalPath;
-    return prevalence(options);
+    (options || (options = {})).path = journalPath
+    return prevalence(options)
   }
 
   function cleanRepo(done) {
     fs.unlink(journalPath)
       .then(done)
-      .catch(done.bind(null,null));
+      .catch(done.bind(null, null))
   }
-});
+})
