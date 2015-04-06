87262b0aa2bbf259b777ce3bafbc45b494d9772c
Joakim Larsson <mimmertim@gmail.com>
2015-04-06 14:36:40 +0200

    Revert 453122e..a022fbc
    This rolls back to commit 453122e59bcee21bc89469ed814cd651fb853973.

--- index.js
+++ index.js
@@ -1,195 +1,243 @@
-'use strict'
+'use strict';
 
-let Promise = require('native-or-bluebird')
-let co = require('co')
-let thenify = require('thenify')
-let costream = require('co-stream')
-let debug = require('debug')('prevalence')
-let RwLock = require('rwlock')
-let defaults = require('defaults')
-let JsonStream = require('json-stream')
-let fs = require('mz/fs')
-let path = require('path')
-let mkdirp = thenify(require('mkdirp'))
-let util = require('util')
-let makeError = require('make-error')
+var Promise = require('native-or-bluebird');
+var co = require('co');
+var thenify = require('thenify');
+var costream = require('co-stream')
+var debug = require('debug')('prevalence');
+var rwlock = require('rwlock');
+var defaults = require('defaults');
+var jsonstream = require('json-stream')
+var fs = require('mz/fs');
+var path = require('path');
+var mkdirp = thenify(require('mkdirp'));
+var util = require('util');
+var makeError = require('make-error');
 
-module.exports = function (options) {
-  return new Repository(options)
+module.exports = function(options) {
+  return new Repository(options);
 }
 
-let ConfigurationError = module.exports.ConfigurationError = makeError('ConfigurationError')
-let CommandError = module.exports.CommandError = makeError('CommandError')
+var ConfigurationError = module.exports.ConfigurationError = makeError('ConfigurationError');
+var CommandError = module.exports.CommandError = makeError('CommandError');
 
-function Repository (options) {
+function Repository(options) {
   options = defaults(options, {
     model: {}
-  })
-  if (!options.path) {
+  });
+  if (!options.path){
     throw new ConfigurationError('Repository requires a path to a journalfile.\r\nUsage: require(\'R\')({path:\'j.log\'})')
   }
 
   // resolve journal path relative to root
-  let journalPath = path.resolve(path.dirname(require.main.filename), options.path)
-  debug('journalling to %s', journalPath)
+  var journalPath = path.resolve( path.dirname(require.main.filename),options.path);
+  debug('journalling to %s', journalPath);
 
-  let model = options.model
-  debug('initial model is %j', model)
+  var model = options.model;
+  debug('initial model is %j', model);
 
-  let lck = new RwLock()
-  let isInitialized = false
-  let commands = {}
+  var lck = new rwlock();
+  var isInitialized = false;
+  var commands = {};
 
-  this.register = register
-  this.query = query
-  this.execute = execute
-
-  function lock (type, fn) {
-    return new Promise(function (resolve, reject) {
-      lck[type](function (release) {
+  function lock(type, fn) {
+    return new Promise(function(resolve, reject) {
+      //debug('%s acquire...', type);
+      lck[type](function(release) {
+        //debug('%s acquired', type);
         co(fn)
-          .then(function (res) {
-            resolve(res)
-            release()
+          .then(function(res) {
+            resolve(res);
+            release();
+            //debug('%s released', type);
           })
-          .catch(function (err) {
-            reject(err)
-            release()
+          .catch(function(err) {
+            reject(err);
+            release();
+            //debug('%s released: %s', type, err);
           })
-      })
+      });
     })
   }
 
-  function readLock (fn) {
-    return lock('readLock', fn)
+  function readLock(fn) {
+    return lock('readLock', fn);
   }
 
-  function writeLock (fn) {
-    return lock('writeLock', fn)
+  function writeLock(fn) {
+    return lock('writeLock', fn);
   }
+/*
+  var pendingJournal = [];
+  function appendToJournal(line) {
+    pendingJournal.push(line);
+    if (pendingJournal.length === 1){
+      requestFlush();
+    }
+    return Promise.resolve(true);
 
-  function appendToJournal (line) {
-    return co(function * () {
+
+    function flush(){
+      co(function*() {
+        debug('flushing to journal: %s entries', pendingJournal.length);
+        pendingJournal.push('');
+        var lines = pendingJournal.join('\r\n');
+        try {
+          return yield fs.appendFile(journalPath, lines, {
+            encoding: 'utf8'
+          });
+        } catch (e) {
+          if (e && e.code === 'ENOENT') {
+            // the journal file doesnt exists nor can it be created
+            // assume its cuz a missing folder
+            debug('creating journalling folder');
+            yield mkdirp(path.dirname(journalPath));
+            return yield fs.appendFile(journalPath, lines, {
+              encoding: 'utf8'
+            });
+          }
+          throw e;
+        }
+      })
+      .then(function (){
+        pendingJournal = [];
+        debug('flushing done');
+      })
+      .then(requestFlush)
+    }
+
+    function requestFlush(){
+      if (pendingJournal.length > 0){
+        setImmediate(flush);
+      }
+    }
+  }
+*/
+  function appendToJournal(line) {
+    return co(function*() {
       try {
-        return yield append()
+        return yield append();
       } catch (e) {
         if (e && e.code === 'ENOENT') {
           // the journal file doesnt exists nor can it be created
           // assume its cuz a missing folder
-          debug('creating journalling folder')
-          yield mkdirp(path.dirname(journalPath))
-          return yield append()
+          debug('creating journalling folder');
+          yield mkdirp(path.dirname(journalPath));
+          return yield append();
         }
-        throw e
+        throw e;
       }
-    })
-    function append () {
+    });
+    function append(){
       return fs.appendFile(journalPath, line + '\n', {
         encoding: 'utf8'
-      })
+      });
     }
   }
 
-  let marshalSkipTypes = {
-    'undefined': true,
-    'boolean': true,
-    'number': true,
-    'string': true
-  }
+  var marshalSkipTypes = {
+    "undefined": true,
+    "boolean": true,
+    "number": true,
+    "string": true
+  };
 
-  function marshal (value) {
-    if ((value === null) || marshalSkipTypes[typeof value]) {
-      return value
+  function marshal(value) {
+    if ((value === null) || marshalSkipTypes[typeof(value)]) {
+      return value;
     }
-    return typeof value.marshal === 'function' ? value.marshal() : JSON.parse(JSON.stringify(value))
+    return typeof(value.marshal) === 'function' ? value.marshal() : JSON.parse(JSON.stringify(value));
   }
 
-  function getCommand (name) {
-    if (commands.hasOwnProperty(name)) {
-      return commands[name]
-    }
-    throw new CommandError(util.format('Unkown command \'%s\'', name))
+  function getCommand(name){
+    if (commands.hasOwnProperty(name)){
+     return commands[name];}
+    throw new CommandError(util.format('Unkown command \'%s\'', name));
   }
 
-  function init () {
+  function init() {
     if (isInitialized) {
-      return Promise.resolve(true)
+      return Promise.resolve(true);
     }
-    return writeLock(function * () {
+    return writeLock(function*() {
       if (isInitialized) {
-        return true
+        return true;
       }
 
-      debug('initializing')
+      debug('initializing');
 
-      let replayCount = 0
+      var replayCount = 0;
 
-      return new Promise(function (resolve, reject) {
-        function onReadStreamError (err) {
-          if (err && err.code === 'ENOENT') {
+      return new Promise(function(resolve, reject) {
+        function onReadStreamError(err) {
+          if (err && err.code == 'ENOENT') {
             debug('no journal to initialize from. skipping.')
-            isInitialized = true
-            return resolve(true)
+            isInitialized = true;
+            return resolve(true);
           }
-          reject(err)
+          reject(err);
         }
 
         fs.createReadStream(journalPath)
           .on('error', onReadStreamError)
-          .pipe(new JsonStream())
+          .pipe(new jsonstream())
           .on('error', reject)
-          .pipe(costream.each(function * (record) {
-            yield getCommand(record.n)(model, record.a, {model: model, name: record.n, arg: record.arg, replay: true})
-            ++replayCount
+          .pipe(costream.each(function*(record) {
+            yield getCommand(record.n)(model, record.a, {model: model, name: record.n, arg: record.arg, replay: true});
+            ++replayCount;
           }, {
             objectMode: true,
             parallell: 1
           }))
           .on('error', reject)
-          .on('finish', function () {
-            isInitialized = true
-            resolve(true)
+          .on('finish', function() {
+            isInitialized = true;
+            resolve(true);
           })
       })
-      .then(function (res) {
-        debug('initialization done. replayed commands: %s', replayCount)
-        return res
+      .then(function (res){
+        debug('initialization done. replayed commands: %s', replayCount);
+        return res;
       })
-      .catch(function (err) {
-        debug('initialization failed: %s', err)
-        throw err
-      })
-    })
+      .catch(function (err){
+        debug('initialization failed: %s', err);
+        throw err;
+      });
+    });
   }
 
-  function register (name, executeFn) {
-    commands[name] = executeFn
-    return this
+  function register(name, executeFn) {
+    commands[name] = executeFn;
+    return this;
   }
 
-  function query (queryFn) {
+  function query(queryFn) {
     return init()
-      .then(function () {
-        return readLock(function * () {
-          let res = yield queryFn(model)
-          return marshal(res)
-        })
-      })
+      .then(function() {
+        return readLock(function*() {
+          var res = yield queryFn(model);
+          return marshal(res);
+        });
+      });
   }
 
-  function execute (name, arg) {
-    let recordData = JSON.stringify({
+  function execute(name, arg) {
+    var recordData = JSON.stringify({
       t: new Date(),
       n: name,
       a: arg
-    })
+    });
     return init()
-      .then(function () {
-        return writeLock(function * () {
-          yield appendToJournal(recordData)
-          let res = yield getCommand(name)(model, arg, {model: model, name: name, arg: arg, replay: false})
-          return marshal(res)
+      .then(function() {
+        return writeLock(function*() {
+          yield appendToJournal(recordData);
+          var res = yield getCommand(name)(model, arg, {model: model, name: name, arg: arg, replay: false});
+          return marshal(res);
         })
-      })
+      });
   }
+
+  this.register = register;
+  this.query = query;
+  this.execute = execute;
 }
